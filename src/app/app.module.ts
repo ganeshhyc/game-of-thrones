15bdb3099ab2e4df1c0d683fb5b1bbf2d2d939c1
Ganesh Kumar <id.ganesh123@gmail.com>
2018-09-05 03:20:31 +0530

    creating all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BookInfoComponent } from './book-info/book-info.component';
 import { HouseInfoComponent } from './house-info/house-info.component';
 import { CharacterInfoComponent } from './character-info/character-info.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,19 @@ import { CharacterInfoComponent } from './character-info/character-info.componen
     HomeComponent,
     BookInfoComponent,
     HouseInfoComponent,
-    CharacterInfoComponent
+    CharacterInfoComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'home',component:HomeComponent},
+      {path:'', redirectTo:'home', pathMatch:'full'},
+      {path:'character/:id', component:CharacterInfoComponent},
+      {path:'book/:id', component:BookInfoComponent},
+      {path:'house/:id', component:HouseInfoComponent},
+      {path:'**',component:NotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
