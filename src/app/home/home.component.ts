7b7a5da5d783d14e1bef045e54eb2b58c73b0ac0
Ganesh Kumar <id.ganesh123@gmail.com>
2018-09-05 05:25:34 +0530

    book cards enabled

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,17 +7,22 @@ import { GotHttpService } from '../got-http.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public books;
 
   constructor(public gotHttpService:GotHttpService) { }
 
   ngOnInit() {
-  }
-  public getbooks = ():any=>{
     this.gotHttpService.getAllBooks().subscribe(
-      data => { console.log(data)},
-      error => {  }
-    )
+             res=>this.books=res
+        ) 
   }
+  // public getbooks = ():any=>{
+  //   return ( 
+  //     this.gotHttpService.getAllBooks().subscribe(
+  //       data => data,
+  //       error => error
+  //   ) )
+  // }
   public getHouses = ():any=>{
     this.gotHttpService.getAllHouses().subscribe(
       data => { console.log(data) },
